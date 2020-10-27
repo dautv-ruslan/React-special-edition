630f7f64ea7b2f891b370967e613c79f1a52ac06
dautv-ruslan <dautv-ruslan@rambler.ru>
2020-10-28 02:21:56 +0300

    First commit
    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                use: [ts-loader],
+            }
+        ]
+    }
+};
